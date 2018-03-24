04d74d50ebbcd394fa1c898fca82f399ced8fa05
Abid Hasan <abidhasan@gmail.com>
2018-03-23 18:56:35 -0600

    Final commit

--- Exercises/Section 25 Lesson 249/index.js
+++ Exercises/Section 25 Lesson 249/index.js
@@ -4,9 +4,17 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    //show the welcome search page
+    res.render("search");
+});
+
 app.get("/results", function(req, res) {
+  //get data from the form
+  const searchParam = req.query.search;
+
   //do the API call
-  request("http://omdbapi.com/?s=california&apikey=thewdb", function(err, resp, body) {
+  request(`http://omdbapi.com/?s=${searchParam}&apikey=thewdb`, function(err, resp, body) {
     if (!err && resp.statusCode == 200) {
       //body is good
       const data = JSON.parse(body);
