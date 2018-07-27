4750caf5ea52e556b093eec1f2f68cf1e8441bbf
Abid Hasan <abidhasan@gmail.com>
2018-07-26 21:23:57 -0600

    Add form validator

--- vue-to-due/src/main.js
+++ vue-to-due/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate'
 
-Vue.config.productionTip = false
+Vue.use(VeeValidate);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
