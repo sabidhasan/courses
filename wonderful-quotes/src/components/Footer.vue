4875ddaab7c2400b9b30cd42a41228c764f10c92
Abid Hasan <abidhasan@gmail.com>
2018-07-31 20:10:30 -0600

    Add footer component

---
+++ wonderful-quotes/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    Info: Click on a quote to delete it
+  </footer>
+</template>
+
+<style scoped>
+   footer {
+    border: 1px solid skyblue; background: lightblue; border-radius: 5px;
+    text-align: center; padding: 15px 0; color: darkblue;
+  }
+</style>
