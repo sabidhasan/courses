0ea33a5355755d87b02c1078344dd28029b13fbe
Abid Hasan <abidhasan@gmail.com>
2018-07-31 20:25:08 -0600

    Add quote component

---
+++ wonderful-quotes/src/components/AddQuote.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <h3>Quote</h3>
+    <textarea v-model="inputQuote"></textarea>
+    <button @click.prevent="addQuote(inputQuote)">Add quote</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'AddQuote',
+    data() {
+      return {
+        inputQuote: 'Some text'
+      }
+    },
+    props: {
+      addQuote: Function
+    }
+  }
+</script>
+
+<style>
+
+</style>
