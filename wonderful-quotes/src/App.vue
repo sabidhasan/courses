4fdd6c5ee365a3845cbdeb2bc21103e5d532c1b5
Abid Hasan <abidhasan@gmail.com>
2018-07-31 20:26:30 -0600

    Update app component to use children

--- wonderful-quotes/src/App.vue
+++ wonderful-quotes/src/App.vue
@@ -1,13 +1,37 @@
 <template>
     <div class="container">
-    
+      <QuoteCounter :length="quotes.length"></QuoteCounter>
+      <AddQuote :addQuote="addQuote"></AddQuote>
+      <Quotes v-for="quote in quotes" :quote="quote"></Quotes>
+      <Footer></Footer>
     </div>
 </template>
 
 <script>
-    export default {
-        
+  import QuoteCounter from './components/QuoteCounter.vue'
+  import AddQuote from './components/AddQuote.vue'
+  import Quotes from './components/Quotes.vue'
+  import Footer from './components/Footer.vue'
+
+  export default {
+    data() {
+      return {
+        quotes: ['a', 'bb']
+      }
+    },
+    methods: {
+      addQuote(quote) {
+        if (!quote) return;
+        this.quotes.push(quote);
+      }
+    },
+    components: {
+      QuoteCounter,
+      AddQuote,
+      Quotes,
+      Footer
     }
+  }
 </script>
 
 <style>
