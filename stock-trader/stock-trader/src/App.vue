fffd356fcc093cbc479f3a0b73c62b99edb79afe
Abid Hasan <abidhasan@gmail.com>
2018-08-09 23:35:53 -0600

    App component has Header

--- stock-trader/stock-trader/src/App.vue
+++ stock-trader/stock-trader/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="container">
+    <Header />
     <router-view/>
   </div>
 </template>
 
 <script>
-  export default {
+  import Header from './components/Header.vue'
 
+  export default {
+    components: {
+      Header,
+    }
   }
 </script>
 
 <style>
-
+  body {
+    padding: 30px;
+  }
 </style>
