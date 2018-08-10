b30263bd6bce2ae13468258757ac0e2cf96b039c
Abid Hasan <abidhasan@gmail.com>
2018-08-09 20:43:44 -0600

    Add basic routing

---
+++ stock-trader/stock-trader/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Header</h1>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- stock-trader/stock-trader/src/router/index.js
+++ stock-trader/stock-trader/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/Home.vue'
+import Portfolio from '../components/portfolio/Portfolio.vue'
+import Stocks from '../components/stocks/Stocks.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    { path: '/', component: Home },
+    { path: '/portfolio', component: Portfolio},
+    { path: '/stocks', component: Stocks},
+  ],
+  mode: 'history'
 })
