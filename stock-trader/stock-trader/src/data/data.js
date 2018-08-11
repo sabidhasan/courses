665aa94630dbc77dc07041cb5f25ed4780bbe4cd
Abid Hasan <abidhasan@gmail.com>
2018-08-11 14:27:15 -0600

    Starting dummy data

---
+++ stock-trader/stock-trader/src/data/data.js
@@ -0,0 +1,6 @@
+export default [
+  {id: 1, name: 'BMW', prices: [110, 100, 90] },
+  {id: 2, name: 'CSI', prices: [60, 110, 50] },
+  {id: 3, name: 'DSK', prices: [90, 90, 60] },
+  {id: 4, name: 'EAC', prices: [120, 70, 80] }
+]
