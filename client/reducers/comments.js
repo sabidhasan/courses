29ebfeaede1844ccd40760e5e1a6fd0d96b0a447
Abid Hasan <abidhasan@gmail.com>
2018-10-23 21:30:10 -0600

    Add action reducers

---
+++ client/reducers/comments.js
@@ -0,0 +1,6 @@
+function comments(state = [], action) {
+  console.log('ssss');
+  return state
+}
+
+export default comments
---
+++ client/reducers/posts.js
@@ -0,0 +1,6 @@
+function posts(state = [], action) {
+  console.log('he');
+  return state
+}
+
+export default posts
