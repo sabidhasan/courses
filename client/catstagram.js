560a7afbafcccac8e80d836fda34c4bb6e479ed0
Abid Hasan <abidhasan@gmail.com>
2018-10-23 21:30:21 -0600

    Add App component

--- client/catstagram.js
+++ client/catstagram.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single'
 import PhotoGrid from './components/PhotoGrid'
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 // import CSS
 import css from './styles/style.styl'
 
 const router = (
-  <Router history={browserHistory} >
-    <Route path='/' component={Main}>
-      <IndexRoute component={PhotoGrid} />
-      <Route path='/view/:postId' component={Single} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history} >
+      <Route path='/' component={App}>
+        <IndexRoute component={PhotoGrid} />
+        <Route path='/view/:postId' component={Single} />
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
