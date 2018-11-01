e8b34ba4262f040db5f32dca72005e48cff0e313
Abid Hasan <abidhasan@gmail.com>
2018-11-01 09:54:15 -0600

    Add photogrid

--- client/components/Main.js
+++ client/components/Main.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { Link } from 'react-router'
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (
       <div>
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Photo from './Photo'
 
 class PhotoGrid extends React.Component {
   render() {
     return (
       <div className='photo-grid'>
-        photo grids
+        {this.props.posts.map((photo, index) => (
+          <Photo
+            key={index}
+            index={index}
+            link={photo.code}
+            src={photo.display_src}
+            caption={photo.caption}
+          />
+        ))}
       </div>
     )
   }
