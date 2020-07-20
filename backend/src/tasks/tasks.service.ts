da1229828bb678fda7b6aca199889e972efa53d0
Abid Hasan <abidhasan@gmail.com>
2020-07-19 19:21:35 -0600

    Support task creation

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
+import * as uuid from 'uuid';
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,19 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const newTask: Task = {
+      id: uuid.v4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(newTask);
+    return newTask;
   }
 }
