4005bb1ad8d7c96c0d6dd74ddf29e3b9e5af52db
Abid Hasan <abidhasan@gmail.com>
2021-01-26 16:19:26 -0800

    Remove uneeded dist dir from VCS

--- code/nest-js/backend/dist/tasks/pipes/task-status-validation.pipe.d.ts
+++
@@ -1,6 +0,0 @@
-import { PipeTransform } from "@nestjs/common";
-export declare class TaskStatusValidationPipe implements PipeTransform {
-    private readonly allowedStatuses;
-    transform(value: string): string;
-    private isStatusValid;
-}
