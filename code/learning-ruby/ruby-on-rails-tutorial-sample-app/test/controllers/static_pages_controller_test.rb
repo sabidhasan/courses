83354294f953838c18d478751453c908b4e5c0fd
Abid Hasan <abidhasan@gmail.com>
2022-10-27 21:03:24 -0700

    Finish static pages

--- code/learning-ruby/ruby-on-rails-tutorial-sample-app/test/controllers/static_pages_controller_test.rb
+++ code/learning-ruby/ruby-on-rails-tutorial-sample-app/test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,37 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Rails Tutorial"
+  end
+
+  test "root route" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
+    assert_select "title", "Home | #{@base_title}"
     assert_response :success
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_select "title", "Help | #{@base_title}"
     assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_select "title", "About | #{@base_title}"
+    assert_response :success
+  end
+
+
+  test "should get contact us" do
+    get static_pages_contact_us_url
+    assert_response :success
+    assert_select "title", "Contact Us | #{@base_title}"
   end
 end
