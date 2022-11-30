2ab9f31e1a113b09dd7bcd7f19ac18cea758567a
Abid Hasan <abidhasan@gmail.com>
2022-11-29 22:25:45 -0800

    Add user validations

--- code/learning-ruby/ruby-on-rails-tutorial-sample-app/app/models/user.rb
+++ code/learning-ruby/ruby-on-rails-tutorial-sample-app/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   validates :name, length: { maximum: 50, minimum: 1 }
-  validates :email, length: { maximum: 128, minimum: 1 }
+  validates :email, length: { maximum: 128, minimum: 1 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_uniqueness_of :email
+
+  before_save :conform_email_case
+
+  def conform_email_case
+    self.email.downcase!
+  end
 end
--- code/learning-ruby/ruby-on-rails-tutorial-sample-app/test/models/user_test.rb
+++ code/learning-ruby/ruby-on-rails-tutorial-sample-app/test/models/user_test.rb
@@ -16,7 +16,44 @@ class UserTest < ActiveSupport::TestCase
 
   test "it fails names that are too long" do
     @test_user.name = "x" * 51
-    puts "VLKAID", @test_user.valid?
     assert !@test_user.valid?
+  end
+
+  test "email validations with valid addresses" do
+    valid_addresses = %w(test@example.com TEST@examPLE.Com te.st@example.co.uk te.st+something@ex.com a_b-c@abc.net)
+
+    valid_addresses.each do |email|
+      @test_user.email = email
+      assert @test_user.valid?, "Email #{email.inspect} should be valid"
+    end
+  end
+
+  test "email validations with invalid addresses" do
+    invalid_addresses = %w(test_example.com test@exam..com TEST@examPLE,Com te.st@example.co. foo@bar+baz.com a_b-)
+
+    invalid_addresses.each do |email|
+      @test_user.email = email
+      assert_not @test_user.valid?, "Email #{email.inspect} should be invalid"
+    end
+  end
+
+  test "it validates uniqueness of the email" do
+    copied_user = @test_user.dup
+    @test_user.save
+    assert_not copied_user.valid?
+  end
+
+  test "it validates uniqueness of the email (case insensitive)" do
+    copied_user = @test_user.dup
+    @test_user.save
+    assert_not copied_user.valid?
+  end
+
+  test "it downcases the email before saving" do
+    mixed_case_email_address = "UpPeRcAsE@exam.com"
+    @test_user.email = mixed_case_email_address
+    @test_user.save
+    @test_user.reload
+    assert_equal @test_user.email, mixed_case_email_address.downcase
   end
 end
